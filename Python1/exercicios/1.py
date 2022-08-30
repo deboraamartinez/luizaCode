2c9f6a4d556fe98b12b38cdd25dc50c52287bc1e
deboramartinez <deboraaspesimartinez@gmail.com>
2022-08-30 19:14:58 -0300

    FEAT: adding exercices python-01

---
+++ Python1/exercicios/1.py
@@ -0,0 +1,12 @@
+print(type(1))
+print(type(12.6))
+print(type(True))
+print(type(False))
+print(type(-543))
+print(type(-5.78))
+
+palavra1 = "copo"
+palavra2 = 'Belo dia'
+
+print(type(palavra1))
+print(type(palavra2))
---
+++ Python1/exercicios/3.py
@@ -0,0 +1,10 @@
+# Operadores Matemáticos
+
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 / 3.0)
+print(13 / 3)
+print(13 / 3.0)
+print(13 // 3.0)
